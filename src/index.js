cc31aa6556ade0612cd7a50d3d4a6ab8428b1366
Aniwesh Kumar <aniweshkumar74@gmail.com>
2023-09-24 00:13:26 +0530

    Added Login page and landing page

--- src/index.js
+++ src/index.js
@@ -5,27 +5,39 @@ import {
 } from 'react-router-dom';
 import { AppProvider } from './context.';
 import './index.css';
+import Nav from "../src/components/Navbar/Navbar";
 import Home from './pages/Home/Home';
 import About from "./pages/About/About";
 import Login from "./pages/Login/Login";
 import Signup from "./pages/Signup/Signup";
 import BookList from "./components/BookList/BookList";
 import BookDetails from "./components/BookDetails/BookDetails";
+import Footer from "./components/footer/footer";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+
   <AppProvider>
+
     <BrowserRouter>
+      <Nav />
+
       <Routes>
-        <Route path="/" element={<Home />}>
+
+        <Route >
+          <Route path="/" element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="book" element={<BookList />} />
           <Route path="/book/:id" element={<BookDetails />} />
           <Route path="login" element={<Login />} />
           <Route path="signup" element={<Signup />} />
         </Route>
+
       </Routes>
+      <Footer />
     </BrowserRouter>
+
   </AppProvider>
 );
 
--- src/pages/Books/Book.jsx
+++ src/pages/Books/Book.jsx
@@ -1,31 +1,21 @@
 import React from 'react';
-// import Header from '../../components/Header/Header';
-import { Outlet } from 'react-router-dom';
-// import Navbar from "../../components/Navbar/Navbar"
-import SearchForm from "../../components/SearchForm/SearchForm";
-import "./home.css"
+import Navbar from "../Navbar/Navbar";
+import SearchForm from "../SearchForm/SearchForm";
+import "./Header.css";
 
-const Home = () => {
+const Header = () => {
   return (
-
-
-
     <div className='holder'>
       <header className='header'>
-
+        <Navbar />
         <div className='header-content flex flex-c text-center text-white'>
           <h2 className='header-title text-capitalize'>find your book of choice.</h2><br />
           <p className='header-text fs-18 fw-3'>“A reader lives a thousand lives before he dies, said Jojen. The man who never reads lives only one.”</p>
           <SearchForm />
         </div>
-
       </header>
-      <Outlet />
     </div>
-
-
-
   )
 }
 
-export default Home
+export default Header
